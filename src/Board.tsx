4e10384be02203fc7de9a78b1653c28c380cfa7a
KushibikiMashu <kushibiki3413@gmail.com>
2020-01-12 01:00:00 +0900

    render winner when game ends

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,12 +1,40 @@
 import React, {useState} from "react";
 import Square from "./Square";
 
+type Squares = Array<string | null>
+
+function calculateWinner(squares: Squares): string | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] &&  squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
 const Board: React.FC = () => {
-  const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null))
+  const [squares, setSquares] = useState<Squares>(Array(9).fill(null))
   const [xIsNext, setNext] = useState<boolean>(true)
 
   function handleClick(i: number) {
     const slicedSquares = squares.slice()
+
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+
     slicedSquares[i] = xIsNext ? 'X' : 'O'
     setSquares(slicedSquares)
     setNext(!xIsNext)
@@ -18,7 +46,14 @@ const Board: React.FC = () => {
       onClick={() => handleClick(i)}
     />
 
-  const status = 'Next player: ' + (xIsNext? 'X': 'O');
+  const winner = calculateWinner(squares)
+
+  let status
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + (xIsNext? 'X': 'O');
+  }
 
   return (
     <div>
