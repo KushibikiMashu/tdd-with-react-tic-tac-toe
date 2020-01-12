ba8903c3aeb95eed3bba46f4306c066678fddf76
KushibikiMashu <kushibiki3413@gmail.com>
2020-01-12 10:22:45 +0900

    Add time travel tests

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -11,7 +11,9 @@ test('render next player is X', () => {
 
 test('render next player is O', () => {
   const {getByText, getAllByRole} = render(<Game/>)
+
   fireEvent.click(getAllByRole('button')[0])
+
   const text = getByText(/Next player: O/i)
   expect(text).toBeInTheDocument()
 })
@@ -29,11 +31,75 @@ test('Square component renders X and O', () => {
 
 test('render Winner X when game ends', () => {
   const {getByText, getAllByRole} = render(<Game/>)
+
   fireEvent.click(getAllByRole('button')[0]) // X
   fireEvent.click(getAllByRole('button')[6]) // O
   fireEvent.click(getAllByRole('button')[1]) // X
   fireEvent.click(getAllByRole('button')[7]) // O
   fireEvent.click(getAllByRole('button')[2]) // X
+
   const text = getByText(/Winner: X/i)
   expect(text).toBeInTheDocument()
+})
+
+test('to have reset button' , () => {
+  const {getByText} = render(<Game/>)
+  const reset = getByText(/Go to game start/i)
+  expect(reset).toBeInTheDocument()
+})
+
+test('reset game', () => {
+  const {getByText, getAllByRole} = render(<Game/>)
+
+  fireEvent.click(getAllByRole('button')[0]) // X
+  const reset = getByText(/Go to game start/i)
+  fireEvent.click(reset)
+
+  expect(getAllByRole('button')[0]).toHaveTextContent('')
+})
+
+test('go back to the first move', () => {
+  const {getByText, getAllByRole} = render(<Game/>)
+
+  fireEvent.click(getAllByRole('button')[0]) // X
+  fireEvent.click(getAllByRole('button')[1]) // O
+  fireEvent.click(getAllByRole('button')[2]) // X
+
+  const moveOne = getByText(/Go to move #1/i)
+  fireEvent.click(moveOne)
+
+  expect(getAllByRole('button')[0]).toHaveTextContent('X')
+  expect(getAllByRole('button')[1]).toHaveTextContent('')
+  expect(getAllByRole('button')[2]).toHaveTextContent('')
+})
+
+test('delete past moves', () => {
+  const {getByText, getAllByRole, queryByLabelText} = render(<Game/>)
+
+  fireEvent.click(getAllByRole('button')[0]) // X
+  fireEvent.click(getAllByRole('button')[1]) // O
+  fireEvent.click(getAllByRole('button')[2]) // X
+
+  const reset = getByText(/Go to game start/i)
+  fireEvent.click(reset)
+
+  fireEvent.click(getAllByRole('button')[0]) // X
+
+  expect(getByText(/Go to move #1/i)).toBeInTheDocument()
+  expect(queryByLabelText(/Go to move #2/i)).toBeNull()
+  expect(queryByLabelText(/Go to move #3/i)).toBeNull()
+})
+
+test('update next player', () => {
+  const {getByText, getAllByRole} = render(<Game/>)
+
+  fireEvent.click(getAllByRole('button')[0]) // X
+  fireEvent.click(getAllByRole('button')[1]) // O
+  fireEvent.click(getAllByRole('button')[2]) // X
+
+  const moveOne = getByText(/Go to move #1/i)
+  fireEvent.click(moveOne)
+
+  const player = getByText(/Next player: O/i)
+  expect(player).toBeInTheDocument()
 })
