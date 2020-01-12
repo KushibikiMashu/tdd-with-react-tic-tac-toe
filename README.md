3fff7f45a37d642dd5191bfd4a939b58f41f3c9b
KushibikiMashu <kushibiki3413@gmail.com>
2020-01-12 14:09:52 +0900

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# tdd-with-react-tic-tac-toe
-
-Implements of [React tutorial](https://ja.reactjs.org/tutorial/tutorial.html) with TypeScript and [react-testing-library](https://github.com/testing-library/react-testing-library).
---
+++ precache-manifest.9fd70353fb2bb67d6da8352c085e4d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5b08e243b0ab5b6a10939eaca57f1d",
+    "url": "/tdd-with-react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "27a2d5e5fe2542252530",
+    "url": "/tdd-with-react-tic-tac-toe/static/css/main.e93e9884.chunk.css"
+  },
+  {
+    "revision": "c344b05ce025702c95b0",
+    "url": "/tdd-with-react-tic-tac-toe/static/js/2.f2b51810.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tdd-with-react-tic-tac-toe/static/js/2.f2b51810.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27a2d5e5fe2542252530",
+    "url": "/tdd-with-react-tic-tac-toe/static/js/main.5281c5fb.chunk.js"
+  },
+  {
+    "revision": "7309588dc5beda6c8fd4",
+    "url": "/tdd-with-react-tic-tac-toe/static/js/runtime-main.4ceabf1d.js"
+  }
+]);
