2a10b4cc5951d2949b5405c366a96db247990453
KushibikiMashu <kushibiki3413@gmail.com>
2020-01-12 14:20:12 +0900

    add link to original code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 import Game from './Game';
 
-const App = () => <Game />;
+const App: React.FC = () => (
+  <>
+    <div style={{ paddingBottom: 16 }}>
+      <a href="https://github.com/KushibikiMashu/tdd-with-react-tic-tac-toe">
+        See codes on GitHub
+      </a>
+    </div>
+    <Game />
+  </>
+);
 
 export default App;
