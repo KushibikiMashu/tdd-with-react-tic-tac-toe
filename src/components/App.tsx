5b9b50d853ed1a3b9d9c0052387608bbd167100f
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-10 02:52:50 +0900

    feat: タイトルを変更

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,13 @@ import Game from './Game';
 
 const App: React.FC = () => (
   <>
-    <div style={{ paddingBottom: 16 }}>
+    <h1>Tic-Tac-Toe</h1>
+    <p>
       <a href="https://github.com/KushibikiMashu/tdd-with-react-tic-tac-toe">
-        See codes on GitHub
+        GitHub
       </a>
-    </div>
+    </p>
+
     <Game />
   </>
 );
