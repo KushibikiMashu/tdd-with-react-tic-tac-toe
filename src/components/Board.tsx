6abe24348337117898a3ebd8544779215e5a7299
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-10 02:53:02 +0900

    refactor: Board コンポーネントをリファクタ

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,42 @@
 import React from 'react';
 import Square from './Square';
-import { Squares } from '../types';
+import {Squares} from '../types';
 
-type Props = {
+type ContainerProps = {
   squares: Squares;
   onClick: (i: number) => void;
   lines: number[] | null;
-};
+}
 
-const Board: React.FC<Props> = ({ squares, onClick, lines }) => {
-  const renderSquare: React.FC<number> = i => (
-    <Square
-      key={i}
-      value={squares[i]}
-      onClick={() => onClick(i)}
-      isCompleted={lines !== null ? lines.includes(i) : false}
-    />
-  );
+type Props = {
+  rows: number[]
+  isWinMark: (i: number) => boolean
+  makeCols: (i: number) => number[]
+} & ContainerProps
 
-  const renderSquares = () => {
-    return [0, 1, 2].map(i => {
-      const multiplied = i * 3;
-      return (
-        <div className="board-row" key={i}>
-          {[multiplied, multiplied + 1, multiplied + 2].map(i =>
-            renderSquare(i)
-          )}
-        </div>
-      );
-    });
-  };
+const Component: React.FC<Props> = (props) => (
+  <>
+    {props.rows.map(i => (
+      <div className="board-row" key={i}>
+        {props.makeCols(i).map(j =>
+          <Square
+            key={j}
+            value={props.squares[j]}
+            onClick={() => props.onClick(j)}
+            isWinMark={props.isWinMark(j)}
+          />
+        )}
+      </div>
+    ))}
+  </>
+)
 
-  return <div>{renderSquares()}</div>;
-};
+const Container: React.FC<ContainerProps> = (props) => {
+  const rows = [0, 1, 2]
+  const makeCols = (i: number) => [i * 3, i * 3 + 1, i * 3 + 2]
+  const isWinMark = (i: number) => props.lines === null ? false : props.lines.includes(i)
 
-export default Board;
+  return <Component {...props} rows={rows} isWinMark={isWinMark} makeCols={makeCols}/>
+}
+
+export default Container;
